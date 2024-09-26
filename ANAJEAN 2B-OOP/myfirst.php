7a614acc6f7510d0f55f7c78ffbeaf919b0bc15b
AnaJean28 <maminsodanajean0@gmail.com>
2024-09-26 17:57:13 +0800

    Add files via upload

---
+++ ANAJEAN 2B-OOP/myfirst.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Name: Ana Jean Maminsod<br>";
+    echo "Address: Carmen<br>";
+    echo "Contact: 123456789<br>";
+    echo "Parents: Rebecca and Dindo Maminsod<br>";
+    ?>
+</body>
+</html>
+
